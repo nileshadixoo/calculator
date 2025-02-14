f86b86d6e4f177196e0fb504cee60804ba56c61f
nileshadixoo <nilesh.chouhan@adixoo.com>
2025-02-14 08:33:33 +0530

    removed eval function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let inputValue = document.getElementById('user-input')
+console.log(inputValue.innerText.includes('%'));
+
 
 
 const numbers = document.querySelectorAll('.key-number').forEach((item) => {
@@ -27,7 +29,91 @@ const calculate = document.querySelectorAll('.operation').forEach((item) => {
             if(inputValue.innerText==0) inputValue.innerText= '0'
         }
         else if(!isNaN(lastValue) && e.target.innerText === '='){
-            inputValue.innerText = eval(inputValue.innerText)
+            // inputValue.innerText = eval(inputValue.innerText)
+            // if(inputValue.innerText.includes('+')){
+            //     let res = (inputValue.innerText.split('+'));
+            //     console.log(res);
+                
+            //     inputValue.innerText = (parseInt(res[0])+parseInt(res[1]))
+            //     console.log(inputValue.innerText);
+                
+
+            // }
+            // else if(inputValue.innerText.includes('-')){
+            //     let res = (inputValue.innerText.split('-'));
+            //     inputValue.innerText = (res[0]-res[1])
+
+            // }
+            // else if(inputValue.innerText.includes('/')){
+            //     let res = (inputValue.innerText.split('/'));
+            //     inputValue.innerText = (res[0]/res[1])
+
+            // }
+            // else if(inputValue.innerText.includes('*')){
+            //     let res = (inputValue.innerText.split('*'));
+            //     inputValue.innerText = (res[0]*res[1])
+
+            // }
+            // else if(inputValue.innerText.includes('%')){
+            //     let res = (inputValue.innerText.split('%'));
+            //     inputValue.innerText = (res[0]%res[1])
+
+            // }
+            function tokenize(s) {
+                // --- Parse a calculation string into an array of numbers and operators
+                const r = [];
+                let token = '';
+                for (const character of s) {
+                    if ('^*/+-'.includes(character)) {
+                        if (token === '' && character === '-') {
+                            token = '-';
+                        } else {
+                            r.push(parseFloat(token), character);
+                            token = '';
+                        }
+                    } else {
+                        token += character;
+                    }
+                }
+                if (token !== '') {
+                    r.push(parseFloat(token));
+                }
+                return r;
+            }
+            
+            function calculate(tokens) {
+              
+                const operatorPrecedence = [{'^': (a, b) => Math.pow(a, b)},
+                           {'*': (a, b) => a * b, '/': (a, b) => a / b},
+                           {'+': (a, b) => a + b, '-': (a, b) => a - b}];
+                let operator;
+                for (const operators of operatorPrecedence) {
+                    const newTokens = [];
+                    for (const token of tokens) {
+                        if (token in operators) {
+                            operator = operators[token];
+                        } else if (operator) {
+                            newTokens[newTokens.length - 1] = 
+                                operator(newTokens[newTokens.length - 1], token);
+                            operator = null;
+                        } else {
+                            newTokens.push(token);
+                        }
+                    }
+                    tokens = newTokens;
+                }
+                if (tokens.length > 1) {
+                    console.log('Error: unable to resolve calculation');
+                    return tokens;
+                } else {
+                    return tokens[0];
+                }
+            }
+            inputValue.innerText = calculate(tokenize(inputValue.innerText))
+            
+            
+
+            
         }
         else if(!isNaN(lastValue)) {
             inputValue.innerText += e.target.innerText
